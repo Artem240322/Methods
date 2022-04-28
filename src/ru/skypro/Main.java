26ba0ac2469cf5f088e64dbb39e263ef6b6abfba
ArtemCH240322 <sun2383@mail.ru>
2022-04-28 22:25:31 +0300

    Первый комит. Лекция практика. ДЗ 1

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,8 +1,20 @@
 package ru.skypro;
+import java.time.Year;
+import java.util.Arrays;
 
-public class Main {
+public class MyFunction {
+    public static void main(String[] args) {...}
 
-    public static void main(String[] args) {
-	// write your code here
+
+    // Задача 1
+    public static void printIsLeapYear(int year) {
+        boolean leapYear = ((year % 4) == 0) && ((year % 100) != 0 || (year % 400) == 0);
+        ;
+        if (leapYear) {
+            System.out.println(year + " высокосный год");
+        } else {
+            System.out.println(year + " не высокосный год");
+
+        }
     }
 }
